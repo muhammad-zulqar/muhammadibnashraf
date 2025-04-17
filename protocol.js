8120e8c0d2436e3a148efcde03df5b7661084ec1
muhammad-zulqar <khanmuhammadzulqarnain34@gmail.com>
2025-04-17 17:56:03 +0530

    Add files via upload

---
+++ protocol.js
@@ -0,0 +1,14 @@
+let a = 09;
+if (a > 18){
+    console.log('You are eligible for driving');
+}else{
+    console.log('You are not eligible for driving');
+}
+alert("Khali Dekh ke chhorna mat Job bhi dena");
+let h1 = document.createElement('h1');
+console.dir(h1);
+let para = document.getElementsByTagName('p');
+console.log(para);
+let text = document.querySelector('p');
+console.log(text.innerText)
+text.innerText = "This is a new text";
